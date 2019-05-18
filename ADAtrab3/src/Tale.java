9bf4abf5270e76af81b6b02beb8fc7f42179f210
denisov93 <43911862+denisov93@users.noreply.github.com>
2019-05-18 20:19:55 +0100

    Update Tale.java

--- ADAtrab3/src/Tale.java
+++ ADAtrab3/src/Tale.java
@@ -1,18 +1,23 @@
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class Tale {
 
-	HashMap<Integer,List<Integer>>friends;
+	Map<Integer,List<Integer>>friends;
 	int[] degree;
 	boolean[] selected;
 	int[] length;
+	int[] noise;
 	int[] via;
 	Queue<int[]> connected;
-	int P, S, T, N;
+	int P;
+	int S;
+	int T;
+	int N;
 	public Tale(int[] degree,int people,int source,int target,int noise) {
 		P = people;
 		S = source;
@@ -23,24 +28,22 @@ public class Tale {
 		selected = new boolean[P];
 		length = new int[P];
 		via = new int[P];
+		this.noise = new int[P];
 		connected = new PriorityQueue<int[]>(P,new CompareLengths());
-		
-		fill();
-	}
-
-	private void fill() {
 		for(int i = 0;i<P;i++) {
 			friends.put(i, new LinkedList<Integer>());
 			selected[i]=false;
 			length[i] = Integer.MAX_VALUE;
 		}
 	}
-	
+
 	public void addFriendship(String[] splited) {
 		int a = Integer.parseInt(splited[0]);
 		int b = Integer.parseInt(splited[1]);
 		friends.get(a).add(b);
 		friends.get(b).add(a);
+		
+		
 	}
 	public int[] result() {
 		dijkstra();
@@ -50,31 +53,51 @@ public class Tale {
 	}
 	
 	private void dijkstra() {
-		length[S]=degree[S];
-		int[]a = {S,degree[S]};
+		length[S]=0;
+		int[]a = {S,0};
 		connected.add(a);
 		do {
 			a = connected.remove();
 			selected[a[0]] = true;
-			exploreNode(a[0]);
+			exploreNode(a);
 		}while(!connected.isEmpty());
 	}
 
-	private void exploreNode(int a) {
-		List<Integer> ls = friends.get(a);
-		for(int i : ls) {System.out.println(i);
+	private void exploreNode(int[] a) {
+		List<Integer> ls = friends.get(a[0]);
+		for(int i : ls) {   //System.out.println(i);
 			if(!selected[i]) {
-				int newL = length[a] + degree[i];
+				int newL = 0;
+					if(a[0]==S) { //amigo de source
+						newL = 0;
+						noise[i]=degree[a[0]];
+					}
+					else {
+						if(a[1]==S) {//amigo de amigo de source
+							newL = length[a[0]];
+							noise[i] = degree[a[1]]*N;
+							
+						}
+						else { //sem cunha
+							noise[i] = degree[a[1]]*noise[a[0]];
+							newL = noise[a[0]] + degree[a[0]] ;
+						
+						}
+					}
+				
 				if(newL < length[i]) {
 					boolean flag = length[i]<Integer.MAX_VALUE;
 					length[i] = newL;
 					//via[i]=a;
 					if(!flag) {
-						int[]b = {length[i],i};
+						int[]b = {i,a[0]};
 						connected.add(b);
 					}
 					else {
-						int[]b = {i,length[i]};
+						int[]b = {i,a[0]};
+						int[] f = connected.remove();
+						if(i!=f[0])System.out.println("Isto é diferente "+i+" de  "+f[0]);
+						System.out.println("ponho "+i);
 						connected.add(b);
 					}
 					
@@ -82,8 +105,6 @@ public class Tale {
 			}
 		}
 	}
-	
-	
 	
 	
 	
